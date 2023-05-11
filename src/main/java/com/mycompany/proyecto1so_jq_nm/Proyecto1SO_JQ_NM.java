fcfa497b3ff08d6c959bb857cf67c4a0d3cf0650
Jenderson Quintero <65185726+JendersonQuintero@users.noreply.github.com>
2023-05-11 11:38:50 -0400

    Primer commit inicio del proyecto

---
+++ src/main/java/com/mycompany/proyecto1so_jq_nm/Proyecto1SO_JQ_NM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1so_jq_nm;
+
+/**
+ *
+ * @author JendersonQ
+ */
+public class Proyecto1SO_JQ_NM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
