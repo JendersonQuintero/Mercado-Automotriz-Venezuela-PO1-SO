43ecd555765f512de5c5eba3f8b462936272b469
Jenderson Quintero <65185726+JendersonQuintero@users.noreply.github.com>
2023-05-31 10:10:43 -0400

    Estructura inicial del proyecto modificada

---
+++ src/main/java/main/vehiclePlantBu/Worker.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.vehiclePlantBu;
+
+/**
+ *
+ * @author JendersonQ
+ */
+public class Worker extends Thread {
+
+    
+
+}
